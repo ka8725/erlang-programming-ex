bebaf71d243977bb87c1e075d69cac48f35a93d8
Andrey Koleshko <ka8725@gmail.com>
2015-12-25 04:32:02 +0300

    Reverse create 3.2

--- 3/test.erl
+++ 3/test.erl
@@ -1,5 +1,5 @@
 -module(test).
--export([sum/1, sum/2]).
+-export([sum/1, sum/2, create/1, reverse_create/1]).
 
 sum(0) -> 0;
 sum(N) -> N + sum(N - 1).
@@ -7,3 +7,9 @@ sum(N) -> N + sum(N - 1).
 sum(N, M) when N > M -> error(badarg);
 sum(X, X) -> X;
 sum(N, M) -> N + sum(N + 1, M).
+
+list_create(0) -> [];
+list_create(N) -> [N|list_create(N - 1)].
+
+create(N) -> lists:reverse(list_create(N)).
+reverse_create(N) -> list_create(N).
