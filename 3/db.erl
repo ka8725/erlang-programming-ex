2980af235d9d74a5d349d2c007d9c45349293341
Andrey Koleshko <ka8725@gmail.com>
2015-12-25 21:33:30 +0300

    Solve task 3-4

---
+++ 3/db.erl
@@ -0,0 +1,20 @@
+-module(db).
+-export([new/0, destroy/1, write/3, delete/2, read/2, match/2]).
+
+new() -> [].
+
+destroy(_) -> ok.
+
+write(Key, Element, Db) -> [{Key, Element}|delete(Key, Db)].
+
+delete(_, Db) when Db =:= [] -> [];
+delete(Key, [{Key, _}|Tail]) -> delete(Key, Tail);
+delete(_, Db) -> Db.
+
+read(_, []) -> {error, instance};
+read(Key, [{Key, Val}|_]) -> {ok, Val};
+read(Key, [_|Tail]) -> read(Key, Tail).
+
+match(_, []) -> [];
+match(Val, [{Key, Val}|Tail]) -> [Key|match(Val, Tail)];
+match(Val, [_|Tail]) -> match(Val, Tail).
