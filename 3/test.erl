bee36c44f865b739af6bb32818679feae72bdae7
Andrey Koleshko <ka8725@gmail.com>
2015-12-25 22:55:39 +0300

    Solution for 3-5

--- 3/test.erl
+++ 3/test.erl
@@ -1,5 +1,6 @@
 -module(test).
--export([sum/1, sum/2, create/1, reverse_create/1, print1/1, print2/1]).
+-export([sum/1, sum/2, create/1, reverse_create/1, print1/1, print2/1,
+         filter/2, reverse/1, concatenate/1, flatten/1]).
 
 sum(0) -> 0;
 sum(N) -> N + sum(N - 1).
@@ -28,3 +29,40 @@ print2(N) when N rem 2 =:= 0 -> print2(N - 1);
 print2(N) when N rem 2 =/= 0 ->
   print2(N - 1),
   io:format("Number: ~p~n", [N]).
+
+
+filter([], _) -> [];
+filter([Head|Tail], N) when Head =< N -> [Head|filter(Tail, N)];
+filter([Head|Tail], N) when Head > N -> filter(Tail, N).
+
+
+reverse(List) ->
+  reverse(List, []).
+
+reverse([], Acc) ->
+  Acc;
+reverse([Head|Tail], Acc) ->
+  reverse(Tail, [Head|Acc]).
+
+
+concatenate(Lists) ->
+  concatenate(Lists, []).
+
+concatenate([], Acc) ->
+  Acc;
+concatenate([[]|Tail], Acc) -> concatenate(Tail, Acc);
+concatenate([[H|T]|Tail], Acc) ->
+  [H|concatenate(T, concatenate(Tail, Acc))];
+concatenate([H|T], Acc) ->
+  [H|concatenate(T, Acc)].
+
+
+flatten(Lists) ->
+  flatten(Lists, []).
+
+flatten([], Acc) ->
+  Acc;
+flatten([Head|Tail], Acc) ->
+  concatenate([flatten(Head, Acc), flatten(Tail, Acc)]);
+flatten(X, Acc) ->
+  [X|Acc].
