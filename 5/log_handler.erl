f2f82857f561641dd21ca3d160ea8053cfa9f7ce
Andrey Koleshko <ka8725@gmail.com>
2016-01-11 03:04:43 +0300

    Swap handlers

---
+++ 5/log_handler.erl
@@ -0,0 +1,26 @@
+%% Code from
+%%   Erlang Programming
+%%   Francecso Cesarini and Simon Thompson
+%%   O'Reilly, 2008
+%%   http://oreilly.com/catalog/9780596518189/
+%%   http:-module(log_handler).
+
+-module(log_handler).
+
+-export([init/1, terminate/1, handle_event/2]).
+
+init(File) ->
+  {ok, Fd} = file:open(File, write),
+  {File, Fd}.
+
+terminate({FileName, Fd}) ->
+  file:close(Fd),
+  FileName.
+
+handle_event({Action, Id, Event}, {_, Fd}) ->
+  {MegaSec, Sec, MicroSec} = now(),
+  Args = io:format(Fd, "~w,~w,~w,~w,~w,~p~n",
+                   [MegaSec, Sec, MicroSec, Action, Id, Event]),
+  Fd;
+handle_event(_, _Data) ->
+  skip.
